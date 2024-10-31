7afb5598e61b9b8aa7857fe1a05441325450b216
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-31 23:04:19 +0100

    Task 3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,43 +5,42 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LRUCache(BaseCaching):
     """
-    LRUCache that inherits from BaseCaching
-    and is a caching system
+    A caching system that inherits from BaseCaching
     """
     def __init__(self):
-        """Initializes the LRUCache Class"""
+        """Initialize"""
         super().__init__()
-        self.order = []
+        self.order = []  # To track order of key accesses for LRU removal algo
 
     def put(self, key, item):
         """
-        Assign to the dictionary self.cache_data the
-        item value for the key key
+        Adds an item in the cache
+
+        Args:
+            key: Key to be used as an index
+            item: Item to be stored
+        Returns:
+            nothing if the key and item is none
         """
-        if (key is None) or (item is None):
-            return
+        if key is not None and item is not None:
+            # If key exists, remove it from order to update its position
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Remove the least recently used item
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD:", lru_key)
 
-        if key in self.cache_data:
-            self.order.remove(key)
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Remove the least recently used item
-            lru_key = self.order.pop(0)
-            del self.cache_data[lru_key]
-            print("DISCARD:", lru_key)
-
-        # Add the new key and item, then update order list
-        self.cache_data[key] = item
-        self.order.append(key)
+            # Add the new key and item, then update order list
+            self.cache_data[key] = item
+            self.order.append(key)
 
     def get(self, key):
-        """
-        Returns the value in self.cache_data linked to key
-        """
-        if key is None:
-            return None
-
-        for ky, val in self.cache_data.items():
-            if ky is None:
-                return None
-            elif ky == key:
-                return val
+        """Gets an item from the cache by key, updating order for LRU"""
+        if key in self.cache_data:
+            # Move accessed key to the end of the order list
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
