4337816e172d19159d6b42751cb12f8adfbb8152
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-05 02:08:22 +0100

    Task 0. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""Parameterize a unit test"""
+"""
+Parameterize a unit test
+"""
 import unittest
-from unittest.mock import Mock, patch
-from utils import access_nested_map
-from parameterized import parameterized, parameterized_class
 from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,14 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
-        self, nested_map: Dict,
-        path: Tuple[str],
-        expected: Union[Dict, int]
-    ) -> None:
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
         """
         A method to test that access_nested_map returns
         what it is supposed to.
