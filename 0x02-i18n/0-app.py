bbdfad8803da5367204df9fbe5f46e9045db123f
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-06 01:47:09 +0100

    Task 0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Setup a basic Flask app in 0-app.py
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
