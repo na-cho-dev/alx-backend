34760ffed9b52c99d56b25234fa0d463801a4377
Fortune Iheanacho <nachodev369@gmail.com>
2024-11-06 01:57:56 +0100

    Task 1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Setup a basic Flask app in 1-app.py
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """Returns the render of 1-index.html"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
